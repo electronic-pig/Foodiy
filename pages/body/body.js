a314b0920f9c6d83ee2350bebb79ebfd8ee2e79a
liyang <2234333815@qq.com>
2023-11-18 11:41:56 +0800

    初步完成首页UI

--- pages/body/body.js
+++ pages/body/body.js
@@ -88,7 +88,20 @@ Page({
       title: '数据提交成功！',
       icon: 'success',
       duration: 2000
-    })
+    });
+    wx.request({
+      url: 'http://127.0.0.1:5000/test', //仅为示例，并非真实的接口地址
+      data: {
+        'x': 'hello'
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    });
   },
   /**
    * 生命周期函数--监听页面加载
@@ -126,19 +139,6 @@ Page({
       bp: [75, 115], //默认值
       bsArray: bsArray,
       bs: 5.0, //默认值
-    });
-    wx.request({
-      url: 'http://127.0.0.1:5000/', //仅为示例，并非真实的接口地址
-      data: {
-        x: '',
-        y: ''
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success (res) {
-        console.log(res.data)
-      }
     });
   },
 

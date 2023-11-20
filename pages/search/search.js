62a4eb3fa28546c3bae1f1cbb47c3945e6a1802b
liyang <2234333815@qq.com>
2023-11-20 16:48:37 +0800

    完成食物页面

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,14 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
+    list: []
+  },
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
 
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    } else {
+      this.setData({
+        modalName: null
+      })
+    }
+    this.setData({
+      ListTouchDirection: null
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let that = this; // 保存页面实例的引用
+    wx.request({
+      url: 'http://127.0.0.1:5000/food/' + options.cata,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // 使用setData将返回的数据保存到页面的data中
+        that.setData({
+          list: res.data
+        });
+      }
+    });
   },
 
   /**
